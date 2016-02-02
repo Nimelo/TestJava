d88acc8b97e4e742934bdb76b99493e564519a91
Mateusz Gąsior <mr.nimelo@gmail.com>
2016-02-02 19:36:23 +0100

    .

---
+++ src/test/java/TestClassTest.java
@@ -0,0 +1,21 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Mateusz on 02.02.2016.
+ */
+public class TestClassTest {
+    @Test
+    public void test1(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(1,2);
+    }
+
+
+}
